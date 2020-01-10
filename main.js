1d7bb2f6adc1241eabf3d450a7fefbd394085896
TKFM21 <tkzwfm21@gmail.com>
2020-01-10 13:54:56 +0900

    demo script completed

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
+const URL = "https://";
+
 (() => {
     const sendBtn = document.getElementById('send');
+    const resultOk = document.getElementById('result-ok');
+    const resultNg = document.getElementById('result-ng');
+    resultOk.hidden = true;
+    resultNg.hidden = true;
 
-    sendBtn.addEventListener('click', event => {
+    sendBtn.addEventListener('click', async event => {
         event.preventDefault();
+        resultOk.hidden = true;
+        resultNg.hidden = true;
         const name = document.getElementById('name').value;
         const email = document.getElementById('email').value;
         const body = document.getElementById('body').value;
         const jsonData = JSON.stringify({ name, email, body });
+        
+        try {
+            const res = await fetch(URL, {
+                method: 'POST',
+                mode: 'cors',
+                cache: 'no-cache',
+                body: jsonData,
+                headers:{
+                  'Content-Type': 'application/json; charset=utf-8'
+                }
+            });
+            console.log('Response!!', res.status);
+            resultOk.hidden = false;
+        } catch (error) {
+            console.log(error);
+            resultNg.hidden = false;
+        }
     });
-
 })();
